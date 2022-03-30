4c2b88f97a77a1b4aceb325fdbdbc865d37d95b5
kyamashiro <kyamashiro73@gmail.com>
2022-03-30 20:33:09 +0900

    kotukotu

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-def main():
-    N = 1
-    max = 0
-    ans = 1
-    for i in range(1, N + 1):
-        tmp = i
-        cnt = 0
-        while True:
-            if tmp % 2 == 0:
-                tmp = tmp / 2
-                cnt += 1
-                if max < cnt:
-                    max = cnt
-                    ans = i
-            else:
-                break
-    print(ans)
-
-
 if __name__ == '__main__':
-    main()
+    S = "different"
+
+    for i in range(len(S)):
+        for j in range(i + 1, len(S)):
+            if S[i] == S[j]:
+                print("no")
+                exit()
+    print("yes")
